2a683bce212ed10a06b4147e9e4b1015bede9d44
Your Name <you@example.com>
2022-05-24 15:08:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let btn = document.getElementById("button");
+let pseudo = doccument.getElementById("pseudo")
+    btn.addEventListener("click",function(){
+        if(pseudo.value === ""){
+            alert("Entrer botre pseudo")
+        }
+    })
+function pseudo(){
+    let input= document.getElementById("input");
+    input.addEventListener("Click", function(){
+
+    })
+}
